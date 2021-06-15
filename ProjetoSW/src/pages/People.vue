ce41e5a694e62c257181d0e45c1c4b29cc31c1f9
thayannecosta <71013147+thayannecosta@users.noreply.github.com>
2021-06-14 21:17:44 -0300

    update

--- ProjetoSW/src/pages/People.vue
+++ ProjetoSW/src/pages/People.vue
@@ -1,10 +1,25 @@
 <template>
   <q-page padding>
     <div class="text-h3 text-center">Personagens</div>
-    <q-item clickable v-ripple v-for="name in people" :key="name.people">
+    <div class="row">
+          <div class="col-md-6" style="margin-left: 15px;">
+            <q-input
+              v-model="buscaNome"
+              debounce="500"
+              filled
+              placeholder="Pesquisa"
+            >
+              <template v-slot:string>
+                <q-icon @click="fazerPesquisa" name="search" />
+              </template>
+            </q-input>
+          </div>
+        </div>
+        <q-item-section>{{buscaNome}}</q-item-section>
+    <q-item clickable v-ripple v-for="person in people" :key="person.name">
         <q-item-section>
-          <q-item-label overline>{{people.name}}</q-item-label>
-          <q-item-label>{{people.birth_year}}</q-item-label>
+          <q-item-label overline>{{person.name}}</q-item-label>
+          <q-item-label>{{person.birth_year}}</q-item-label>
         </q-item-section>
         <q-item-section thumbnail>
           <img src="">
@@ -13,21 +28,27 @@
   </q-page>
 </template>
 
-// v-for="people in people" :key="people.name"> {{people.name}}
 <script>
 export default {
   name: 'People',
   data () {
     return {
-      people: {}
-
+      people: {},
+      buscaNome: ''
     }
   },
   methods: {
     listarPersonagens () {
-      this.$axios.get('https://swapi.dev/api/people/1')
+      this.$axios.get('https://swapi.dev/api/people')
         .then(response => {
-          this.people = (response.data)
+          this.people = response.data.results
+        })
+    },
+    fazerPesquisa () {
+      const url = 'https://swapi.dev/api/people/?search=${this.buscaNome}'
+      this.$axios.get(url)
+        .then(response => {
+          this.buscaNome = response.data.results
         })
     }
   },
